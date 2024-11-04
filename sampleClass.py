ca6df37ea39faf6d742beb258d9910ef7395c9fb
Stoli04 <kakoa14@aol.com>
2024-11-04 00:03:55 -0800

    Update sampleClass.py

--- sampleClass.py
+++ sampleClass.py
@@ -1,1 +1,20 @@
+class PartyAnimal:
 
+    def __init__(self):
+        self.x = 0
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far...", self.x)
+
+
+an = PartyAnimal()
+
+print("type", type(an))
+print("dir ", dir(an))
+print("Type", type(an.x))
+print("Type", type(an.party))
+
+an.party()
+an.party()
+an.party()
